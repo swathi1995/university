9ec1bd2bb8b907c8761e8d489f5b888dd15a4a96
swathi1995 <swathi.vply@gmail.com>
2016-02-05 13:06:19 +0530

    first commit

--- myuniversity/settings.py
+++ myuniversity/settings.py
@@ -120,8 +120,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
 
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (MEDIA_ROOT,)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")

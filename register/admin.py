72a7ea04c2a3693ebb6d36317e3873c9f3d09f33
swathi1995 <swathi.vply@gmail.com>
2016-02-06 14:53:53 +0530

    aded image

--- register/admin.py
+++ register/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib import admin
+from register.models import *
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(User, UserAdmin)
+
 
 # Register your models here.
